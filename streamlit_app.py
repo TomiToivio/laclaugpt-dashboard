ba271a85872971677e1335f9461381734c64653a
TomiToivio <tomi.toivio@helsinki.fi>
2025-06-26 11:32:38 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,19 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.set_page_config(
+    page_title="LaclauGPT Demo",
+    page_icon=":alien:",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'mailto:tomi.toivio@helsinki.fi',
+        'Report a bug': "mailto:tomi.toivio@helsinki.fi",
+        'About': "LaclauGPT by Tomi Toivio."
+    }
 )
+
+st.title("LaclauGPT Dev App")
+st.write("This is a simple Streamlit app to demonstrate LaclauGPT's capabilities.")
+
