7693b3a78b74e751a937677dcabfff49a3752e38
Tomi Toivio <tomi.toivio@helsinki.fi>
2025-06-26 16:27:27 +0300

    Add function to generate random text comments
    Introduces create_random_text() to generate random comment-like text using a selection of words. This can be used for testing or dummy data generation.

--- dummy_generator.py
+++ dummy_generator.py
@@ -13,3 +13,11 @@ def create_random_username():
     """Generate a random username."""
     return f"user_{random.randint(1, 1000)}"
 
+def create_random_text():
+    """Generate a random text comment."""
+    words = ["great", "video", "interesting", "love", "hate", "amazing", "boring", "funny", "sad", "happy"]
+    return ' '.join(random.choices(words, k=random.randint(5, 15)))
+
+
+
+# id, url, username, date, text, analysis, sentiment_positive, sentiment_negative, sentiment_neutral, topics, entities
