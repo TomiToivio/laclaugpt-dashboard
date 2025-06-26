8826f363704cbd9883330316aee2491f0e71e585
Tomi Toivio <tomi.toivio@helsinki.fi>
2025-06-26 17:41:16 +0300

    Add new feature to user profile page
    Implemented additional functionality on the user profile page, including support for editing user details and updating the profile picture. Refactored related components for better maintainability.

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,28 @@ df = load_data()
 st.title("LaclauGPT - Social Media Video Analysis")
 st.subheader("Analyze and visualize social media video data with LaclauGPT")
 
-st.dataframe(df)
+search_query = st.sidebar.text_input("Search:")
+
+start_date = st.sidebar.date_input("Start date", value=df["status_date"].min())
+end_date = st.sidebar.date_input("End date", value=df["status_date"].max())
+
+# COnvert 'status_date' to datetime if not already
+if not pd.api.types.is_datetime64_any_dtype(df['status_date']):
+    df['status_date'] = pd.to_datetime(df['status_date'], errors='coerce')
+
+filtered_df = df.copy()
+
+filtered_df = filtered_df[(filtered_df["status_date"] >= pd.to_datetime(start_date)) & (filtered_df["status_date"] <= pd.to_datetime(end_date))]
+
+if search_query:
+    filtered_df = filtered_df[filtered_df.apply(lambda row: row.astype(str).str.contains(search_query, case=False, na=False).any(), axis=1)]
+
+st.dataframe(filtered_df)
+
+total_count = len(df)
+filtered_count = len(filtered_df)
+st.write(f"Viewing {filtered_count} out of {total_count} statuses.")
+
 
 tab1, tab2, tab3 = st.tabs(["Sentiments", "Topics", "Entities"])
 with tab1:
