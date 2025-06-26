730925617a188514528a4a627718256133b7b72b
Tomi Toivio <tomi.toivio@helsinki.fi>
2025-06-26 16:06:10 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,19 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 
 st.set_page_config(
-    page_title="LaclauGPT Demo",
+    page_title="LaclauGPT - Social Media Video Analysis",
     page_icon=":alien:",
     layout="wide",
     initial_sidebar_state="expanded",
     menu_items={
         'Get Help': 'https://github.com/TomiToivio/laclaugpt-dashboard-development',
         'Report a bug': "https://github.com/TomiToivio/laclaugpt-dashboard-development/issues",
-        'About': "LaclauGPT by Tomi Toivio."
+        'About': "Developed by Tomi Toivio, University of Helsinki."
     }
 )
 
-st.title("LaclauGPT Dev App")
-st.write("This is a simple Streamlit app to demonstrate LaclauGPT's capabilities.")
+st.title("LaclauGPT - Social Media Video Analysis")
+st.subheader("Analyze and visualize social media video data with LaclauGPT")
 
+st.caption("LaclauGPT - Social Media Video Analysis")
